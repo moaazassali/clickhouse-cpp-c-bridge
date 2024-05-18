63ec43007849613e694fbdfa1f5e3d35021edb4a
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-05-18 11:52:20 +0400

    added simple tests in main

--- src/clickhouse-cpp-c-bridge.cpp
+++ src/clickhouse-cpp-c-bridge.cpp
@@ -17,3 +17,47 @@
 #include "columns/column_string.h"
 #include "columns/column_datetime.h"
 #include "columns/column_datetime64.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!dsvdsv" << std::endl;
+    const auto client = CreateClient("192.168.70.176");
+
+    std::cout << "Created client" << std::endl;
+
+    /// Create a table.
+    const auto query = CreateQuery("DROP TABLE test.gg");
+    const auto [code, message] = Execute(client, query);
+
+    std::cout << code << " " << message << std::endl;
+
+    /*
+    Execute(client, "CREATE TABLE IF NOT EXISTS default.numbers (id UInt64, name String) ENGINE = Memory");
+
+    auto block = CreateBlock();
+
+    auto idCol = CreateColumnUInt64();
+    ColumnUInt64Append(*idCol, 1);
+    ColumnUInt64Append(*idCol, 7);
+
+
+    auto nameCol = CreateColumnString();
+    ColumnStringAppend(*nameCol, "one");
+    ColumnStringAppend(*nameCol, "seven");
+
+    AppendColumn(block, "id", idCol);
+    AppendColumn(block, "name", nameCol);
+
+    Insert(client, "default.numbers", block);
+
+    FreeColumn(idCol);
+    FreeColumn(nameCol);
+    FreeBlock(block);
+    */
+
+    // keep the console window open
+    std::cin.get();
+
+    return 0;
+}
