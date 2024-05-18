74bb400edd44d4fb59c1f4d04d4538dc3ff3012a
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-05-18 23:19:09 +0400

    fixed value being passed by reference for datetime and datetime64

--- src/columns/column_datetime.h
+++ src/columns/column_datetime.h
@@ -8,6 +8,6 @@ extern "C" __declspec(dllexport) inline ColumnDateTime * CreateColumnDateTime() 
 	return new ColumnDateTime();
 }
 
-extern "C" __declspec(dllexport) inline void ColumnDateTimeAppend(ColumnDateTime & column, const time_t & value) {
-	column.Append(value);
+extern "C" __declspec(dllexport) inline void ColumnDateTimeAppend(ColumnDateTime * column, const time_t value) {
+	column->Append(value);
 }
--- src/columns/column_datetime64.h
+++ src/columns/column_datetime64.h
@@ -8,6 +8,6 @@ extern "C" __declspec(dllexport) inline ColumnDateTime64 * CreateColumnDateTime6
 	return new ColumnDateTime64(precision);
 }
 
-extern "C" __declspec(dllexport) inline void ColumnDateTime64Append(ColumnDateTime64 & column, const int64_t & value) {
-	column.Append(value);
+extern "C" __declspec(dllexport) inline void ColumnDateTime64Append(ColumnDateTime64 * column, const int64_t value) {
+	column->Append(value);
 }
