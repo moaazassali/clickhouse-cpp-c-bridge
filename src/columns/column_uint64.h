332cd0a9b32bff8d723d22fead74e10f8157b4c0
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-05-27 14:58:58 +0400

    Update column_uint64.h

--- src/columns/column_uint64.h
+++ src/columns/column_uint64.h
@@ -4,10 +4,10 @@
 
 using namespace clickhouse;
 
-extern "C" EXPORT inline ColumnUInt64 * CreateColumnUInt64() {
-	return new ColumnUInt64();
+extern "C" EXPORT inline ColumnUInt64 *CreateColumnUInt64() {
+    return new ColumnUInt64();
 }
 
-extern "C" EXPORT inline void ColumnUInt64Append(ColumnUInt64 * column, const uint64_t value) {
-	column->Append(value);
+extern "C" EXPORT inline void ColumnUInt64Append(ColumnUInt64 *column, const uint64_t value) {
+    column->Append(value);
 }
