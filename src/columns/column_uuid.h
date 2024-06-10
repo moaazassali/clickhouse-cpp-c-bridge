c0450bf70e6aaf9f2b0d68a048c3c50a55b466dc
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-10 21:48:50 +0400

    added uuid struct and At() support for uuid column

--- src/columns/column_uuid.h
+++ src/columns/column_uuid.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <clickhouse/columns/numeric.h>
+#include "../structs/uuid_wrapper.h"
 
 using namespace clickhouse;
 
@@ -10,4 +10,9 @@ extern "C" EXPORT inline ColumnUUID *CreateColumnUUID() {
 
 extern "C" EXPORT inline void ColumnUUIDAppend(ColumnUUID *column, const uint64_t value1, const uint64_t value2) {
     column->Append(std::make_pair(value1, value2));
+}
+
+extern "C" EXPORT inline uuid_wrapper ColumnUUIDAt(const ColumnUUID *column, const size_t index) {
+    const auto [first, second] = column->At(index);
+    return uuid_wrapper{first, second};
 }
---
+++ src/structs/uuid_wrapper.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+struct uuid_wrapper {
+    uint64_t first;
+    uint64_t second;
+};
