bf7664846f5c9bb65ba4125e9a276cd0e38f152d
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-13 22:34:14 +0400

    added support for GetColumnType() for any column pointer

--- src/columns/column_wrapper.h
+++ src/columns/column_wrapper.h
@@ -2,6 +2,10 @@
 
 using namespace clickhouse;
 
-extern "C" EXPORT inline void FreeColumn(const Column * col) {
-	delete col;
+extern "C" EXPORT inline void FreeColumn(const Column *col) {
+    delete col;
+}
+
+extern "C" EXPORT inline int32_t GetColumnType(const Column *col) {
+    return col->Type()->GetCode();
 }
