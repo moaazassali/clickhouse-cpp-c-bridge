48f971456c458d55f0a2ee0b3aa77ea476b4d04d
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-23 02:21:17 +0400

    added support for column reserve and clear methods

--- src/columns/column_wrapper.h
+++ src/columns/column_wrapper.h
@@ -9,3 +9,11 @@ extern "C" EXPORT inline void FreeColumn(const Column *col) {
 extern "C" EXPORT inline int32_t GetColumnType(const Column *col) {
     return col->Type()->GetCode();
 }
+
+extern "C" EXPORT inline void ReserveColumn(Column *col, const size_t new_cap) {
+    col->Reserve(new_cap);
+}
+
+extern "C" EXPORT inline void ClearColumn(Column *col) {
+    col->Clear();
+}
