534fba41a7e264df51cbc49d9e86633a4db8e2dd
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-22 20:06:22 +0400

    added support for ipv6 column

---
+++ src/columns/column_ipv6.h
@@ -0,0 +1,20 @@
+#pragma once
+
+using namespace clickhouse;
+
+extern "C" EXPORT inline ColumnIPv6 *CreateColumnIPv6() {
+    return new ColumnIPv6();
+}
+
+extern "C" EXPORT inline void ColumnIPv6Append(ColumnIPv6 *column, const unsigned char value[16]) {
+    in6_addr addr{};
+    for (int i = 0; i < 16; i++) {
+        addr.s6_addr[i] = value[i];
+    }
+    column->Append(addr);
+}
+
+// returns a 16-byte array
+extern "C" EXPORT inline unsigned char *ColumnIPv6At(const ColumnIPv6 *column, const size_t index) {
+    return column->At(index).s6_addr;
+}
