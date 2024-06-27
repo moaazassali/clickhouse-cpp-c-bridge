15e6f05bfbd7ed2881e3113f1904f08e593e1a3f
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-27 15:54:29 +0400

    Added FreeOptionalWrapper function since new Nullable API uses pointer to struct

--- src/structs/optional_wrapper.h
+++ src/structs/optional_wrapper.h
@@ -34,3 +34,77 @@ typedef OptionalWrapper(double, OptionalFloat64Wrapper);
 typedef OptionalWrapper(UUIDWrapper, OptionalUUIDWrapper);
 
 typedef OptionalWrapper(const unsigned char *, OptionalIPv6Wrapper);
+
+extern "C" EXPORT inline void FreeOptionalWrapper(const void *wrapper, const Type::Code code) {
+    switch (code) {
+        case Type::UInt8:
+            delete static_cast<const OptionalUInt8Wrapper *>(wrapper);
+            break;
+        case Type::UInt16:
+            delete static_cast<const OptionalUInt16Wrapper *>(wrapper);
+            break;
+        case Type::UInt32:
+            delete static_cast<const OptionalUInt32Wrapper *>(wrapper);
+            break;
+        case Type::UInt64:
+            delete static_cast<const OptionalUInt64Wrapper *>(wrapper);
+            break;
+        case Type::Int8:
+            delete static_cast<const OptionalInt8Wrapper *>(wrapper);
+            break;
+        case Type::Int16:
+            delete static_cast<const OptionalInt16Wrapper *>(wrapper);
+            break;
+        case Type::Int32:
+            delete static_cast<const OptionalInt32Wrapper *>(wrapper);
+            break;
+        case Type::Int64:
+            delete static_cast<const OptionalInt64Wrapper *>(wrapper);
+            break;
+        case Type::Int128:
+            delete static_cast<const OptionalInt128Wrapper *>(wrapper);
+            break;
+        case Type::UUID:
+            delete static_cast<const OptionalUUIDWrapper *>(wrapper);
+            break;
+        case Type::Float32:
+            delete static_cast<const OptionalFloat32Wrapper *>(wrapper);
+            break;
+        case Type::Float64:
+            delete static_cast<const OptionalFloat64Wrapper *>(wrapper);
+            break;
+        case Type::Decimal:
+            delete static_cast<const OptionalInt128Wrapper *>(wrapper);
+            break;
+        case Type::Date:
+            delete static_cast<const OptionalUInt16Wrapper *>(wrapper);
+            break;
+        case Type::Date32:
+            delete static_cast<const OptionalInt32Wrapper *>(wrapper);
+            break;
+        case Type::DateTime:
+            delete static_cast<const OptionalUInt32Wrapper *>(wrapper);
+            break;
+        case Type::DateTime64:
+            delete static_cast<const OptionalInt64Wrapper *>(wrapper);
+            break;
+        case Type::Enum8:
+            delete static_cast<const OptionalInt8Wrapper *>(wrapper);
+            break;
+        case Type::Enum16:
+            delete static_cast<const OptionalInt16Wrapper *>(wrapper);
+            break;
+        case Type::String:
+        case Type::FixedString:
+            delete static_cast<const OptionalStringViewWrapper *>(wrapper);
+            break;
+        case Type::IPv4:
+            delete static_cast<const OptionalUInt32Wrapper *>(wrapper);
+            break;
+        case Type::IPv6:
+            delete static_cast<const OptionalIPv6Wrapper *>(wrapper);
+            break;
+        default:
+            break;
+    }
+}
