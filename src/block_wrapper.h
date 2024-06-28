3453c566acbc651e50918ca590139087c519660f
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-28 23:22:45 +0400

    added comment explaining the decision for why columns are not owned by the block

--- src/block_wrapper.h
+++ src/block_wrapper.h
@@ -5,52 +5,45 @@
 
 using namespace clickhouse;
 
-extern "C" EXPORT inline Block * CreateBlock()
-{
-	return new Block();
+extern "C" EXPORT inline Block *CreateBlock() {
+    return new Block();
 }
 
-extern "C" EXPORT inline void FreeBlock(const Block * block)
-{
-	delete block;
+extern "C" EXPORT inline void FreeBlock(const Block *block) {
+    delete block;
 }
 
-extern "C" EXPORT inline void AppendColumn(Block * block, const char* name, Column * col)
-{
-	// fake shared_ptr to comply with the method signature
-	block->AppendColumn(name, make_fake_shared(col));
+extern "C" EXPORT inline void AppendColumn(Block *block, const char *name, Column *col) {
+    // fake shared_ptr to comply with the method signature
+    // freeing the columns should be handled by the external library since columns are directly used by the end user.
+    // we don't want them to be owned by the block and freed automatically when the user might still have references to them.
+    block->AppendColumn(name, make_fake_shared(col));
 }
 
-extern "C" EXPORT inline size_t GetColumnCount(const Block * block)
-{
-	return block->GetColumnCount();
+extern "C" EXPORT inline size_t GetColumnCount(const Block *block) {
+    return block->GetColumnCount();
 }
 
 extern "C" EXPORT inline BlockInfo Info(const Block *block) {
-	return block->Info();
+    return block->Info();
 }
 
-extern "C" EXPORT inline void SetInfo(Block * block, BlockInfo info)
-{
-	block->SetInfo(info);
+extern "C" EXPORT inline void SetInfo(Block *block, BlockInfo info) {
+    block->SetInfo(info);
 }
 
-extern "C" EXPORT inline size_t GetRowCount(const Block * block)
-{
-	return block->GetRowCount();
+extern "C" EXPORT inline size_t GetRowCount(const Block *block) {
+    return block->GetRowCount();
 }
 
-extern "C" EXPORT inline size_t RefreshRowCount(Block * block)
-{
-	return block->RefreshRowCount();
+extern "C" EXPORT inline size_t RefreshRowCount(Block *block) {
+    return block->RefreshRowCount();
 }
 
-extern "C" EXPORT inline const char* GetColumnName(const Block * block, const size_t idx)
-{
-	return block->GetColumnName(idx).c_str();
+extern "C" EXPORT inline const char *GetColumnName(const Block *block, const size_t idx) {
+    return block->GetColumnName(idx).c_str();
 }
 
-extern "C" EXPORT inline Column * GetColumnAt(const Block * block, const size_t idx)
-{
-	return (*block)[idx].get();
+extern "C" EXPORT inline Column *GetColumnAt(const Block *block, const size_t idx) {
+    return (*block)[idx].get();
 }
