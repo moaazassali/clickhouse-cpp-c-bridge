127ae21221e06d93079a5e023c18ef74241e7263
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-30 01:30:30 +0400

    updated uuid column tests to use library's ColumnXAt() instead of original col->At()

--- tests/columns/column_uuid_tests.cpp
+++ tests/columns/column_uuid_tests.cpp
@@ -26,22 +26,22 @@ TEST_CASE("Appending to and retrieving from ColumnUUID correctly") {
     uint64_t second = dis(gen);
 
     ColumnUUIDAppend(col, {first, second});
-    CHECK(col->At(0).first == first);
+    CHECK(ColumnUUIDAt(col, 0).first == first);
     CHECK(col->At(0).second == second);
     CHECK(col->Size() == 1);
 
     uint64_t max_uint64 = std::numeric_limits<uint64_t>::max();
     ColumnUUIDAppend(col, {max_uint64, max_uint64});
     SUBCASE("Returns same value when appending uuid max value") {
-        CHECK(col->At(1).first == max_uint64);
-        CHECK(col->At(1).second == max_uint64);
+        CHECK(ColumnUUIDAt(col, 1).first == max_uint64);
+        CHECK(ColumnUUIDAt(col, 1).second == max_uint64);
         CHECK(col->Size() == 2);
     }
 
     ColumnUUIDAppend(col, {0, 0});
     SUBCASE("Returns same value when appending uuid min value") {
-        CHECK(col->At(2).first == 0);
-        CHECK(col->At(2).second == 0);
+        CHECK(ColumnUUIDAt(col, 2).first == 0);
+        CHECK(ColumnUUIDAt(col, 2).second == 0);
         CHECK(col->Size() == 3);
     }
 }
