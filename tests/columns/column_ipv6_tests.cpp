e22d887caf1a8aced2f40e6df895aa49d38f575c
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-01 02:22:38 +0400

    added tests for column ipv6

---
+++ tests/columns/column_ipv6_tests.cpp
@@ -0,0 +1,30 @@
+#include <doctest/doctest.h>
+#include <clickhouse/types/types.h>
+
+#include "columns/column_ipv6.h"
+
+TEST_CASE("Constructed ColumnIPv6 is valid") {
+    const auto col = CreateColumnIPv6();
+
+    SUBCASE("Type is IPv6") {
+        CHECK(col->Type()->GetCode() == Type::IPv6);
+    }
+
+    SUBCASE("Size is 0") {
+        CHECK(col->Size() == 0);
+    }
+}
+
+TEST_CASE("Appending to and retrieving from ColumnIPv6 correctly") {
+    const auto col = CreateColumnIPv6();
+
+    unsigned char value[16] = {
+        0x00, 0x01, 0x02, 0x03,
+        0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0A, 0x0B,
+        0x0C, 0x0D, 0x0E, 0x0F
+    };
+    ColumnIPv6Append(col, value);
+    CHECK(memcmp(ColumnIPv6At(col, 0), value, 16) == 0);
+    CHECK(col->Size() == 1);
+}
