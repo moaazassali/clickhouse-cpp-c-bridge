e05518f7646004ccb1f083c7550ee21923b90a40
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-01 02:54:53 +0400

    iwyu updates to string and fixed string columns

--- src/columns/column_fixed_string.h
+++ src/columns/column_fixed_string.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <clickhouse/columns/string.h>
+
+#include "export.h"
+#include "structs/string_view_wrapper.h"
+#include "structs/clickhouse_result_status.h"
+
 using namespace clickhouse;
 
 extern "C" EXPORT inline ColumnFixedString *CreateColumnFixedString(const size_t n) {
@@ -16,78 +22,4 @@ extern "C" EXPORT inline ClickHouseResultStatus ColumnFixedStringAppend(ColumnFi
 extern "C" EXPORT inline StringViewWrapper ColumnFixedStringAt(const ColumnFixedString *column, const size_t index) {
     auto sv = column->At(index);
     return {sv.data(), sv.length()};
-}
-
-// ========================================
-// LowCardinality(FixedString)
-// ========================================
-extern "C" EXPORT inline ColumnLowCardinalityT<ColumnFixedString> *
-CreateColumnLowCardinality_FixedString(const size_t n) {
-    return new ColumnLowCardinalityT<ColumnFixedString>(n);
-}
-
-extern "C" EXPORT inline ClickHouseResultStatus ColumnLowCardinality_FixedString_Append(
-    ColumnLowCardinalityT<ColumnFixedString> *column,
-    const char *value) {
-    return TryCatchClickHouseError([&]() {
-        column->Append(value);
-    });
-}
-
-extern "C" EXPORT inline StringViewWrapper ColumnLowCardinality_FixedString_At(
-    const ColumnLowCardinalityT<ColumnFixedString> *column,
-    const size_t index) {
-    auto sv = column->At(index);
-    return {sv.data(), sv.length()};
-}
-
-// ========================================
-// Nullable(FixedString)
-// ========================================
-extern "C" EXPORT inline ColumnNullableT<ColumnFixedString> *CreateColumnNullable_FixedString(const size_t n) {
-    return new ColumnNullableT<ColumnFixedString>(n);
-}
-
-extern "C" EXPORT inline ClickHouseResultStatus ColumnNullable_FixedString_Append(
-    ColumnNullableT<ColumnFixedString> *column,
-    const char *value) {
-    return TryCatchClickHouseError([&]() {
-        column->Append(value);
-    });
-}
-
-extern "C" EXPORT inline OptionalStringViewWrapper ColumnNullable_FixedString_At(
-    const ColumnNullableT<ColumnFixedString> *column,
-    const size_t index
-) {
-    const auto value = column->At(index);
-    return value.has_value()
-               ? OptionalStringViewWrapper{true, StringViewWrapper{value.value().data(), value.value().length()}}
-               : OptionalStringViewWrapper{false, StringViewWrapper{nullptr, 0}};
-}
-
-// ========================================
-// LowCardinality(Nullable(FixedString))
-// ========================================
-extern "C" EXPORT inline ColumnLowCardinalityT<ColumnNullableT<ColumnFixedString> > *
-CreateColumnLowCardinality_Nullable_FixedString(const size_t n) {
-    return new ColumnLowCardinalityT<ColumnNullableT<ColumnFixedString> >(n);
-}
-
-extern "C" EXPORT inline ClickHouseResultStatus ColumnLowCardinality_Nullable_FixedString_Append(
-    ColumnLowCardinalityT<ColumnNullableT<ColumnFixedString> > *column,
-    const char *value) {
-    return TryCatchClickHouseError([&]() {
-        column->Append(value);
-    });
-}
-
-extern "C" EXPORT inline OptionalStringViewWrapper ColumnLowCardinality_Nullable_FixedString_At(
-    const ColumnLowCardinalityT<ColumnNullableT<ColumnFixedString> > *column,
-    const size_t index
-) {
-    const auto value = column->At(index);
-    return value.has_value()
-               ? OptionalStringViewWrapper{true, StringViewWrapper{value.value().data(), value.value().length()}}
-               : OptionalStringViewWrapper{false, StringViewWrapper{nullptr, 0}};
 }
--- src/columns/column_string.h
+++ src/columns/column_string.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <clickhouse/columns/string.h>
+
+#include "export.h"
 #include "structs/string_view_wrapper.h"
 
 using namespace clickhouse;
@@ -20,67 +22,4 @@ extern "C" EXPORT inline void ColumnStringAppend(ColumnString *column, const cha
 extern "C" EXPORT inline StringViewWrapper ColumnStringAt(const ColumnString *column, const size_t index) {
     auto sv = column->At(index);
     return {sv.data(), sv.length()};
-}
-
-// ==================================
-// LowCardinality(String)
-// ==================================
-extern "C" EXPORT inline ColumnLowCardinalityT<ColumnString> *CreateColumnLowCardinality_String() {
-    return new ColumnLowCardinalityT<ColumnString>();
-}
-
-extern "C" EXPORT inline void ColumnLowCardinality_String_Append(ColumnLowCardinalityT<ColumnString> *column,
-                                                                 const char *value) {
-    column->Append(value);
-}
-
-extern "C" EXPORT inline StringViewWrapper ColumnLowCardinality_String_At(
-    const ColumnLowCardinalityT<ColumnString> *column,
-    const size_t index) {
-    auto sv = column->At(index);
-    return {sv.data(), sv.length()};
-}
-
-// ==================================
-// Nullable(String)
-// ==================================
-extern "C" EXPORT inline ColumnNullableT<ColumnString> *CreateColumnNullable_String() {
-    return new ColumnNullableT<ColumnString>();
-}
-
-extern "C" EXPORT inline void ColumnNullable_String_Append(ColumnNullableT<ColumnString> *column, const char *value) {
-    column->Append(value);
-}
-
-extern "C" EXPORT inline OptionalStringViewWrapper ColumnNullable_String_At(
-    const ColumnNullableT<ColumnString> *column,
-    const size_t index
-) {
-    const auto value = column->At(index);
-    return value.has_value()
-               ? OptionalStringViewWrapper{true, StringViewWrapper{value.value().data(), value.value().length()}}
-               : OptionalStringViewWrapper{false, StringViewWrapper{nullptr, 0}};
-}
-
-// ==================================
-// LowCardinality(Nullable(String))
-// ==================================
-extern "C" EXPORT inline ColumnLowCardinalityT<ColumnNullableT<ColumnString> > *
-CreateColumnLowCardinality_Nullable_String() {
-    return new ColumnLowCardinalityT<ColumnNullableT<ColumnString> >();
-}
-
-extern "C" EXPORT inline void ColumnLowCardinality_Nullable_String_Append(
-    ColumnLowCardinalityT<ColumnNullableT<ColumnString> > *column,
-    const char *value) {
-    column->Append(value);
-}
-
-extern "C" EXPORT inline OptionalStringViewWrapper ColumnLowCardinality_Nullable_String_At(
-    const ColumnLowCardinalityT<ColumnNullableT<ColumnString> > *column,
-    const size_t index) {
-    const auto value = column->At(index);
-    return value.has_value()
-               ? OptionalStringViewWrapper{true, StringViewWrapper{value.value().data(), value.value().length()}}
-               : OptionalStringViewWrapper{false, StringViewWrapper{nullptr, 0}};
 }
