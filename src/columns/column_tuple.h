ab9cc9e4e64809867eea39acc9d68cee6d10cc0c
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-01 16:03:46 +0400

    added safe guards for invalid arguments in column tuple creation

--- src/columns/column_tuple.h
+++ src/columns/column_tuple.h
@@ -1,24 +1,41 @@
 #pragma once
 
+#include <cstdarg>
+#include <clickhouse/columns/tuple.h>
+
+#include "export.h"
+#include "utils.h"
+#include "structs/clickhouse_result_status.h"
+
 using namespace clickhouse;
 
 // We use a fake shared_ptr to give the external library full control over the columns.
 // However, these columns should probably not be directly exposed to the end user.
 // Instead, the library should create them based on the passed types by the user and only expose ColumnTuple.
 // But there might be a use case for exposing the columns, so we won't limit the functionality here.
-extern "C" EXPORT inline ColumnTuple *CreateColumnTuple(int n, ...) {
-    std::vector<ColumnRef> columns;
+extern "C" EXPORT inline ClickHouseResultStatus CreateColumnTuple(ColumnTuple **outColumn, int n, ...) {
     va_list args;
     va_start(args, n);
 
-    for (int i = 0; i < n; ++i) {
-        Column *col = va_arg(args, Column*);
-        columns.push_back(make_fake_shared(col));
-    }
+    return TryCatchClickHouseError([&]() {
+        if (n <= 0) {
+            throw ValidationError("Tuple size should be greater than 0");
+        }
 
-    va_end(args);
+        std::vector<ColumnRef> columns;
 
-    return new ColumnTuple(columns);
+        for (int i = 0; i < n; ++i) {
+            Column *col = va_arg(args, Column*);
+            if (col == nullptr) {
+                throw ValidationError("Argument " + std::to_string(i + 3) + " is nullptr, not a valid column pointer");
+            }
+            columns.push_back(make_fake_shared(col));
+        }
+
+        va_end(args);
+
+        *outColumn = new ColumnTuple(columns);
+    });
 }
 
 extern "C" EXPORT inline Column *ColumnTupleColumnAt(const ColumnTuple *column, const size_t index) {
