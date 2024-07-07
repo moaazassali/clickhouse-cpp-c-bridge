f0ded69e90f0b26f4c837ca8b71e0ebed562abe3
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-07 19:57:06 +0400

    added support for all query callbacks (not exception) and tests

--- src/query_wrapper.h
+++ src/query_wrapper.h
@@ -14,6 +14,14 @@ extern "C" EXPORT inline void chc_query_free(const Query *query) {
     delete query;
 }
 
+extern "C" EXPORT inline const char *chc_query_get_text(const Query *query) {
+    return query->GetText().c_str();
+}
+
+extern "C" EXPORT inline const char *chc_query_get_query_id(const Query *query) {
+    return query->GetQueryID().c_str();
+}
+
 inline SelectCallback GetSelectCallback(void (*cb)(const Block *block)) {
     return [cb](const Block &block) {
         cb(&block);
@@ -34,18 +42,42 @@ extern "C" EXPORT inline void chc_query_on_data_cancelable(Query *query, bool (*
     query->OnDataCancelable(GetSelectCancelableCallback(cb));
 }
 
-// typedef bool (*SelectServerLogCallbackWrapper)(const Block *block);
-//
-// extern "C" EXPORT inline void QueryOnSelectServerCallback(Query *query, SelectServerLogCallbackWrapper cb) {
-//     query->OnServerLog([&cb](const Block &block) {
-//         return cb(&block);
-//     });
-// }
-//
-// typedef bool (*ProfileEventsCallbackWrapper)(const Block *block);
-//
-// extern "C" EXPORT inline void QueryOnProfileEventsCallback(Query *query, ProfileEventsCallbackWrapper cb) {
-//     query->OnProfileEvents([&cb](const Block &block) {
-//         return cb(&block);
-//     });
-// }
+inline ProgressCallback GetProgressCallback(void (*cb)(const Progress *progress)) {
+    return [cb](const Progress &progress) {
+        cb(&progress);
+    };
+}
+
+extern "C" EXPORT inline void chc_query_on_progress(Query *query, void (*cb)(const Progress *progress)) {
+    query->OnProgress(GetProgressCallback(cb));
+}
+
+inline SelectServerLogCallback GetSelectServerLogCallback(bool (*cb)(const Block *block)) {
+    return [cb](const Block &block) {
+        return cb(&block);
+    };
+}
+
+extern "C" EXPORT inline void chc_query_on_server_log(Query *query, bool (*cb)(const Block *block)) {
+    query->OnServerLog(GetSelectServerLogCallback(cb));
+}
+
+inline ProfileEventsCallback GetProfileEventsCallback(bool (*cb)(const Block *block)) {
+    return [cb](const Block &block) {
+        return cb(&block);
+    };
+}
+
+extern "C" EXPORT inline void chc_query_on_profile_events(Query *query, bool (*cb)(const Block *block)) {
+    query->OnProfileEvents(GetProfileEventsCallback(cb));
+}
+
+inline ProfileCallbak GetProfileCallback(void (*cb)(const Profile *profile)) {
+    return [cb](const Profile &profile) {
+        cb(&profile);
+    };
+}
+
+extern "C" EXPORT inline void chc_query_on_profile(Query *query, void (*cb)(const Profile *profile)) {
+    query->OnProfile(GetProfileCallback(cb));
+}
--- tests/query_wrapper_tests.cpp
+++ tests/query_wrapper_tests.cpp
@@ -10,6 +10,21 @@ TEST_CASE("Correctly create and free query") {
     CHECK_NOTHROW(chc_query_free(q));
 }
 
+TEST_CASE("Correctly get query text") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CHECK(std::string(chc_query_get_text(q)) == query);
+}
+
+TEST_CASE("Correctly get query id") {
+    const auto query = "SELECT 1";
+    const auto query_id = "123";
+    Query *q = chc_query_create(query, query_id);
+    CHECK(q != nullptr);
+    CHECK(std::string(chc_query_get_query_id(q)) == query_id);
+}
+
 static bool CALLED = false;
 
 static void FakeCallback(const Block *block) {
@@ -52,4 +67,86 @@ TEST_CASE("Correctly set query on data cancelable callback") {
     Query *q = chc_query_create(query);
     CHECK(q != nullptr);
     CHECK_NOTHROW(chc_query_on_data_cancelable(q, FakeCancelableCallback));
+}
+
+static void FakeProgressCallback(const Progress *progress) {
+    CALLED = true;
+}
+
+TEST_CASE("Correctly convert and execute ProgressCallbackWrapper") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CALLED = false;
+    const auto cb = GetProgressCallback(FakeProgressCallback);
+    cb(Progress()); // call the fake callback
+    CHECK(CALLED);
+}
+
+TEST_CASE("Correctly set query on progress callback") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CHECK_NOTHROW(chc_query_on_progress(q, FakeProgressCallback));
+}
+
+static bool FakeSelectServerLogCallback(const Block *block) {
+    return true;
+}
+
+TEST_CASE("Correctly convert and execute SelectServerLogCallbackWrapper") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+
+    const auto cb = GetSelectServerLogCallback(FakeSelectServerLogCallback);
+    CHECK(cb(Block()) == true); // call the fake callback
+}
+
+TEST_CASE("Correctly set query on select server log callback") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CHECK_NOTHROW(chc_query_on_server_log(q, FakeSelectServerLogCallback));
+}
+
+static bool FakeProfileEventsCallback(const Block *block) {
+    return true;
+}
+
+TEST_CASE("Correctly convert and execute ProfileEventsCallbackWrapper") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+
+    const auto cb = GetProfileEventsCallback(FakeProfileEventsCallback);
+    CHECK(cb(Block()) == true); // call the fake callback
+}
+
+TEST_CASE("Correctly set query on profile events callback") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CHECK_NOTHROW(chc_query_on_profile_events(q, FakeProfileEventsCallback));
+}
+
+static void FakeProfileCallback(const Profile *profile) {
+    CALLED = true;
+}
+
+TEST_CASE("Correctly convert and execute ProfileCallbackWrapper") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CALLED = false;
+    const auto cb = GetProfileCallback(FakeProfileCallback);
+    cb(Profile()); // call the fake callback
+    CHECK(CALLED);
+}
+
+TEST_CASE("Correctly set query on profile callback") {
+    const auto query = "SELECT 1";
+    Query *q = chc_query_create(query);
+    CHECK(q != nullptr);
+    CHECK_NOTHROW(chc_query_on_profile(q, FakeProfileCallback));
 }
