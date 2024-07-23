ed5bb0b0ff9a36c970123f263b62a67f48955dcf
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-23 22:09:05 +0400

    fixed null pointer reference to column type name

--- src/columns/column_wrapper.h
+++ src/columns/column_wrapper.h
@@ -15,7 +15,11 @@ extern "C" EXPORT inline int32_t chc_column_type_code(const Column *col) {
 }
 
 extern "C" EXPORT inline const char *chc_column_type_name(const Column *col) {
-    return col->Type()->GetName().c_str();
+    // Allocate memory and copy string contents
+    const auto typeName = col->Type()->GetName();
+    const auto result = new char[typeName.length() + 1];
+    strcpy(result, typeName.c_str());
+    return result;
 }
 
 extern "C" EXPORT inline void chc_column_reserve(Column *col, const size_t new_cap) {
